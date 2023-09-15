f22bdfa9b1acdb2c8c70364b5ce874603cbe4965
ninegak <zxce2546@gmail.com>
2023-09-15 12:18:30 +0700

    change directory of picture

--- src/components/Reservation.vue
+++ src/components/Reservation.vue
@@ -80,14 +80,14 @@ const money = ref(0);
 
   //const tlAttraction = ref(['ท่าช้าง', 'GoodViews', 'RiverSide', 'WarmUp', 'ID4'])
 const tlAttraction = ref([
-    { name:'ท่าช้าง', price:80, size: { s: 80, m: 150, l: 200 }, img: "src/assets/tahchang.jpg" }, 
-    { name:'GoodViews', price:120, size: { s: 80, m: 150, l: 250 }, img: "src/assets/goodviews.jpg" }, 
-    { name:'RiverSide', price:150, size: { s: 80, m: 150, l: 300 }, img: "src/assets/riverside.jpg" }, 
-    { name:'WarmUp', price:100, size: { s: 80, m: 150, l: 500 }, img: "src/assets/warmup.jpg" }, 
-    { name:'THAY Nimman', price:90, size: { s: 80, m: 150, l: 700 }, img: "src/assets/thaynimman.jpg" },
-    { name:'Lism Cafe', price:90, size: { s: 80, m: 150, l: 700 }, img: "src/assets/lism.jpg" },
-    { name:'Beer Lab', price:90, size: { s: 80, m: 150, l: 700 }, img: "src/assets/beerlab.jpg" },
-    { name:'TAMMA Cafe', price:90, size: { s: 80, m: 150, l: 700 }, img: "src/assets/tamma.jpg" }
+    { name:'ท่าช้าง', price:80, size: { s: 80, m: 150, l: 200 }, img: "https://www.awaygpub.com/wp-content/uploads/2021/03/%E0%B8%97%E0%B9%88%E0%B8%B2%E0%B8%8A%E0%B9%89%E0%B8%B2%E0%B8%87%E0%B8%84%E0%B8%B2%E0%B9%80%E0%B8%9F%E0%B9%88-%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%99%E0%B9%80%E0%B8%AB%E0%B8%A5%E0%B9%89%E0%B8%B2%E0%B8%8A%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%94%E0%B8%B1%E0%B8%87%E0%B9%83%E0%B8%88%E0%B8%81%E0%B8%A5%E0%B8%B2%E0%B8%87%E0%B9%80%E0%B8%A1%E0%B8%B7%E0%B8%AD%E0%B8%87%E0%B9%83%E0%B8%AB%E0%B8%A1%E0%B9%88-awaygpub-3.jpg" }, 
+    { name:'GoodViews', price:120, size: { s: 80, m: 150, l: 250 }, img: "https://www.chillnaid.com/wp-content/uploads/2017/08/P8220053-1024x768.jpg" }, 
+    { name:'RiverSide', price:150, size: { s: 80, m: 150, l: 300 }, img: "http://www.dooasia.com/wp-content/uploads/2018/05/The-RiverSide-%E0%B9%80%E0%B8%8A%E0%B8%B5%E0%B8%A2%E0%B8%87%E0%B9%83%E0%B8%AB%E0%B8%A1%E0%B9%88-7.jpg" }, 
+    { name:'WarmUp', price:100, size: { s: 80, m: 150, l: 500 }, img: "https://pbs.twimg.com/media/EIhrLy9U8AEdpUl?format=jpg&name=large" }, 
+    { name:'THAY Nimman', price:90, size: { s: 80, m: 150, l: 700 }, img: "https://scontent.fbkk29-8.fna.fbcdn.net/v/t39.30808-6/275553294_5447999895223902_8749872957228405946_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=49d041&_nc_eui2=AeEtKuerdYNz5dSuF8ZX1MwgX-Daxlw776Zf4NrGXDvvpg2jJFyn78TRGStcpmcaiObMHRkit2XONItgBS-9tFZv&_nc_ohc=fPkZSb_2BZwAX__g6jl&_nc_ht=scontent.fbkk29-8.fna&oh=00_AfD4M2E-zJcCvlnDcTt6REOCWcg129JJw49EOntogr6yDA&oe=65089127" },
+    { name:'Lism Cafe', price:90, size: { s: 80, m: 150, l: 700 }, img: "https://img.wongnai.com/p/1920x0/2019/07/14/7e31672612ba4462b77d77cd41ca83e7.jpg" },
+    { name:'Beer Lab', price:90, size: { s: 80, m: 150, l: 700 }, img: "https://img.wongnai.com/p/1920x0/2016/08/25/9998216c3b5f4670a016571ed558ab6c.jpg" },
+    { name:'TAMMA Cafe', price:90, size: { s: 80, m: 150, l: 700 }, img: "https://scontent.fbkk29-7.fna.fbcdn.net/v/t39.30808-6/275609725_2224948664309812_6792567553159522822_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5614bc&_nc_eui2=AeE6N7qrwWoByAJT0YeOxjKBUUvIys2tUyBRS8jKza1TIAJHeP1JLfdrczNlflD-yuhREhBk_8mFHJ2nqL66QBT5&_nc_ohc=sOLirVbae6QAX-B2aSc&_nc_ht=scontent.fbkk29-7.fna&oh=00_AfD9YPdF3AfoGJfIvqZ-TNVCFJmv3pLKpVUgevw063XEzw&oe=650990C6" }
   ]); 
 
 const tableData = ref([]); // Create a ref for the table data
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,41 +5,31 @@ document.body.classList.add('page-a-body');
 <template>
   <section>
   <div id="carouselContain" class="carousel slide" style="margin-top: 100px;">
-    <div class="carousel-indicators" >
-      <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-      <button type="button" data-bs-target="#carousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
-      <button type="button" data-bs-target="#carousel" data-bs-slide-to="4" aria-label="Slide 5"></button>
-      <button type="button" data-bs-target="#carousel" data-bs-slide-to="5" aria-label="Slide 6"></button>
-      <button type="button" data-bs-target="#carousel" data-bs-slide-to="6" aria-label="Slide 7"></button>
-      <button type="button" data-bs-target="#carousel" data-bs-slide-to="7" aria-label="Slide 8"></button>
-    </div>
     <div id="carouselControl" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active" data-bs-interval="10000">
-        <img src="src/assets/tahchang.jpg" class="d-block w-100" alt="...">
+        <img src="https://www.awaygpub.com/wp-content/uploads/2021/03/%E0%B8%97%E0%B9%88%E0%B8%B2%E0%B8%8A%E0%B9%89%E0%B8%B2%E0%B8%87%E0%B8%84%E0%B8%B2%E0%B9%80%E0%B8%9F%E0%B9%88-%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%99%E0%B9%80%E0%B8%AB%E0%B8%A5%E0%B9%89%E0%B8%B2%E0%B8%8A%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%94%E0%B8%B1%E0%B8%87%E0%B9%83%E0%B8%88%E0%B8%81%E0%B8%A5%E0%B8%B2%E0%B8%87%E0%B9%80%E0%B8%A1%E0%B8%B7%E0%B8%AD%E0%B8%87%E0%B9%83%E0%B8%AB%E0%B8%A1%E0%B9%88-awaygpub-3.jpg" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item" data-bs-interval="2000">
-        <img src="src/assets/goodviews.jpg" class="d-block w-100" alt="...">
+        <img src="https://www.chillnaid.com/wp-content/uploads/2017/08/P8220053-1024x768.jpg" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="src/assets/riverside.jpg" class="d-block w-100" alt="...">
+        <img src="http://www.dooasia.com/wp-content/uploads/2018/05/The-RiverSide-%E0%B9%80%E0%B8%8A%E0%B8%B5%E0%B8%A2%E0%B8%87%E0%B9%83%E0%B8%AB%E0%B8%A1%E0%B9%88-7.jpg" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="src/assets/warmup.jpg" class="d-block w-100" alt="...">
+        <img src="https://pbs.twimg.com/media/EIhrLy9U8AEdpUl?format=jpg&name=large" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="src/assets/thaynimman.jpg" class="d-block w-100" alt="...">
+        <img src="https://scontent.fbkk29-8.fna.fbcdn.net/v/t39.30808-6/275553294_5447999895223902_8749872957228405946_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=49d041&_nc_eui2=AeEtKuerdYNz5dSuF8ZX1MwgX-Daxlw776Zf4NrGXDvvpg2jJFyn78TRGStcpmcaiObMHRkit2XONItgBS-9tFZv&_nc_ohc=fPkZSb_2BZwAX__g6jl&_nc_ht=scontent.fbkk29-8.fna&oh=00_AfD4M2E-zJcCvlnDcTt6REOCWcg129JJw49EOntogr6yDA&oe=65089127" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="src/assets/lism.jpg" class="d-block w-100" alt="...">
+        <img src="https://img.wongnai.com/p/1920x0/2019/07/14/7e31672612ba4462b77d77cd41ca83e7.jpg" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="src/assets/beerlab.jpg" class="d-block w-100" alt="...">
+        <img src="https://img.wongnai.com/p/1920x0/2016/08/25/9998216c3b5f4670a016571ed558ab6c.jpg" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="src/assets/tamma.jpg" class="d-block w-100" alt="...">
+        <img src="https://scontent.fbkk29-7.fna.fbcdn.net/v/t39.30808-6/275609725_2224948664309812_6792567553159522822_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5614bc&_nc_eui2=AeE6N7qrwWoByAJT0YeOxjKBUUvIys2tUyBRS8jKza1TIAJHeP1JLfdrczNlflD-yuhREhBk_8mFHJ2nqL66QBT5&_nc_ohc=sOLirVbae6QAX-B2aSc&_nc_ht=scontent.fbkk29-7.fna&oh=00_AfD9YPdF3AfoGJfIvqZ-TNVCFJmv3pLKpVUgevw063XEzw&oe=650990C6" class="d-block w-100" alt="...">
       </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselControl" data-bs-slide="prev">
